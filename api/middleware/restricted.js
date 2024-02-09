dd47a445cd11d67e3a7018137c3bca7ae3fda4ec
Ben Frame <brewerytattooer@gmail.com>
2024-02-09 09:13:56 -0500

    wip

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,14 +1,29 @@
-module.exports = (req, res, next) => {
-  next();
-  /*
-    IMPLEMENT
+const validateUsername = (req, res, next) => {
+  if(req.body.username === req.body.username){
+    next({
+      
+      message: 'username taken'
+    })
+  } else {
 
-    1- On valid token in the Authorization header, call next.
+    next();
+  }
+}
 
-    2- On missing token in the Authorization header,
-      the response body should include a string exactly as follows: "token required".
 
-    3- On invalid or expired token in the Authorization header,
-      the response body should include a string exactly as follows: "token invalid".
-  */
-};
+
+module.exports = {
+  validateUsername
+}
+
+/*
+  IMPLEMENT
+
+  1- On valid token in the Authorization header, call next.
+
+  2- On missing token in the Authorization header,
+    the response body should include a string exactly as follows: "token required".
+
+  3- On invalid or expired token in the Authorization header,
+    the response body should include a string exactly as follows: "token invalid".
+*/
