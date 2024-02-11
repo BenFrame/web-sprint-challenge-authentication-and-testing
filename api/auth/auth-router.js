0d8ccd541eb7b8d15bb7614e7586ac02b4e4f2df
Ben Frame <brewerytattooer@gmail.com>
2024-02-11 16:05:52 -0500

    wip

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -63,7 +63,7 @@ router.post('/register',  tempMiddleware, async ( req, res, next ) => {
   */
 });
 
-router.post('/login', checkUsernameAndPassword, checkUsernameExists, restricted, async (req, res, next) => {
+router.post('/login', checkUsernameAndPassword, checkUsernameExists,  async (req, res, next) => {
   if(bcrypt.compareSync(req.body.password, req.user.password)){
     const token = buildToken(req.user)
     
